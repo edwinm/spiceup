db679e1056cc49a9a0be8979e7e68a52b4337736
Edwin Martin <edwin@bitstorm.org>
2017-11-04 22:45:15 +0100

    Added sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function () {
     return gulp.src('./src/main.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./dest'));
 });
 
